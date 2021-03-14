d2d22c76385327547da3495695efaa5f9ee9b1d8
vostrikov2014 <vostrikov2014@gmail.com>
2021-03-14 23:07:58 +0300

    data entry income/expenses

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,6 @@
 class Currency < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :wallets, dependent: :destroy
+  has_many :rates, dependent: :destroy
   validates :name, :code, presence: true
 end
